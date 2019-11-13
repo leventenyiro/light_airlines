5ad8525ee4a434487e9db861739c59a716ea0e68
Leviii99 <nyiro.levente@gmail.com>
2019-11-13 11:17:30 +0100

    loadingactivity lottiefiles

--- app/src/main/java/com/example/szakdolgozat/LoadingActivity.java
+++ app/src/main/java/com/example/szakdolgozat/LoadingActivity.java
@@ -2,6 +2,7 @@ package com.example.szakdolgozat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,15 +13,14 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
-
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        
         Thread welcomeThread = new Thread() {
 
             @Override
             public void run() {
                 try {
                     super.run();
-                    sleep(1000);  //Delay of 1 seconds
+                    sleep(5000);  //Delay of 1 seconds = 1000 millis
                 } catch (Exception e) {
 
                 } finally {
