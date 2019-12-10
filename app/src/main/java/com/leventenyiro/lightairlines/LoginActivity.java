d72e26bacae585580dda436764025dcf47c606cf
Leviii99 <nyiro.levente@gmail.com>
2019-12-10 12:42:50 +0100

    sikeres login után finish

--- app/src/main/java/com/leventenyiro/lightairlines/LoginActivity.java
+++ app/src/main/java/com/leventenyiro/lightairlines/LoginActivity.java
@@ -81,6 +81,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     intent = new Intent(LoginActivity.this, MainInnerActivity.class);
                     startActivity(intent);
                     overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                    finish();
                 }
                 else if (!login())
                 {
