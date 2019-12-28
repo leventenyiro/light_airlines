51fd92164a6a437cd70b37ef084d5d8d98e9a3e5
Leviii99 <nyiro.levente@gmail.com>
2019-12-28 14:53:31 +0100

    PasswordUpdate létrehozása

--- app/src/main/java/com/leventenyiro/lightairlines/PasswordUpdate.java
+++ app/src/main/java/com/leventenyiro/lightairlines/PasswordUpdate.java
@@ -3,12 +3,92 @@ package com.leventenyiro.lightairlines;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class PasswordUpdate extends AppCompatActivity {
+public class PasswordUpdate extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText inputOldPassword, inputPassword, inputPasswordAgain;
+    private Button btnCancel, btnUpdate;
+    private ImageView btnBack;
+    private Database db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passwordupdate);
+
+        init();
+
+        inputOldPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                inputOldPassword.setBackground(getResources().getDrawable(R.drawable.input));
+                inputOldPassword.setPaddingRelative(70, 40, 40, 40);
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+        inputPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                inputPassword.setBackground(getResources().getDrawable(R.drawable.input));
+                inputPassword.setPaddingRelative(70, 40, 40, 40);
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+        inputPasswordAgain.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                inputPasswordAgain.setBackground(getResources().getDrawable(R.drawable.input));
+                inputPasswordAgain.setPaddingRelative(70, 40, 40, 40);
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+
+        btnBack.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+        btnUpdate.setOnClickListener(this);
+    }
+
+    private void init() {
+        inputOldPassword = findViewById(R.id.inputOldPassword);
+        inputPassword = findViewById(R.id.inputPassword);
+        inputPasswordAgain = findViewById(R.id.inputPasswordAgain);
+        btnBack = findViewById(R.id.btnBack);
+        btnCancel = findViewById(R.id.btnCancel);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        db = new Database(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.btnBack: onBackPressed(); break;
+            case R.id.btnCancel: onBackPressed(); break;
+            case R.id.btnUpdate:
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 }
