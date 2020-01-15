c48d3a94b0f46036714ba3161ad1093e58ae80d9
Leviii99 <nyiro.levente@gmail.com>
2020-01-15 18:58:57 +0100

    Jegyek fragment kialakítása

--- app/src/main/java/com/leventenyiro/lightairlines/fragments/JegyekFragment.java
+++ app/src/main/java/com/leventenyiro/lightairlines/fragments/JegyekFragment.java
@@ -1,19 +1,189 @@
 package com.leventenyiro.lightairlines.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.leventenyiro.lightairlines.R;
+import com.leventenyiro.lightairlines.segedOsztalyok.Database;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JegyekFragment extends Fragment {
 
+    private Context mContext;
+    private Database db;
+    private List<Integer> cardLista;
+    private RelativeLayout mRelativeLayout;
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_jegyek, container, false);
+
+        db = new Database(getActivity());
+        cardLista = new ArrayList<>();
+        mContext = root.getContext();
+        mRelativeLayout = root.findViewById(R.id.relativeLayout);
+
+        select();
+
+
+
         return root;
+    }
+
+    private void select() {
+        for (int i : cardLista) {
+            CardView c = mRelativeLayout.findViewById(i);
+            mRelativeLayout.removeView(c);
+        }
+        cardLista.clear();
+
+        Cursor eredmeny = db.selectJegyek(getActivity().getSharedPreferences("variables", Context.MODE_PRIVATE).getString("userId", ""));
+        String foglalasId;
+        String ules;
+        String idopont;
+        String indulas;
+        String celallomas;
+        String idotartam;
+        if (eredmeny != null && eredmeny.getCount() > 0) {
+            int id = 0;
+            while (eredmeny.moveToNext()) {
+                foglalasId = eredmeny.getString(0);
+                ules = eredmeny.getString(1);
+                idopont = eredmeny.getString(2);
+                indulas = eredmeny.getString(3);
+                celallomas = eredmeny.getString(4);
+                idotartam = eredmeny.getString(5);
+
+                CardView card = new CardView(mContext);
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(dpToPx(360), dpToPx(200));
+                params.addRule(RelativeLayout.CENTER_HORIZONTAL);
+                if (cardLista.size() == 0)
+                    params.addRule(RelativeLayout.BELOW, R.id.textCim);
+                else
+                    params.addRule(RelativeLayout.BELOW, id);
+                if (eredmeny.getCount() - 1 == cardLista.size())
+                    params.setMargins(0,0,0,dpToPx(100));
+                else
+                    params.setMargins(0,0,0,dpToPx(20));
+                card.setLayoutParams(params);
+                card.setCardElevation(50);
+                card.setBackground(getResources().getDrawable(R.drawable.bg_card));
+                final String finalFoglalasId = foglalasId;
+                card.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("variables", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("foglalasId", finalFoglalasId);
+                        editor.apply();
+                        // itt megy majd át
+                        /*Intent intent = new Intent(getActivity(), .class);
+                        startActivity(intent);
+                        getActivity().overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);*/
+                    }
+                });
+                card.setId(card.generateViewId());
+                id = card.getId();
+                cardLista.add(id);
+
+                RelativeLayout rlCard = new RelativeLayout(mContext);
+                RelativeLayout.LayoutParams paramsRlCard = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                rlCard.setLayoutParams(paramsRlCard);
+
+                TextView tvVaros = new TextView(mContext);
+                RelativeLayout.LayoutParams paramsVaros = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                paramsVaros.topMargin = dpToPx(20);
+                tvVaros.setLayoutParams(paramsVaros);
+                tvVaros.setText(indulas + " - " + celallomas);
+                tvVaros.setTypeface(getActivity().getResources().getFont(R.font.regular));
+                tvVaros.setTextColor(getActivity().getResources().getColor(R.color.gray));
+                tvVaros.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                tvVaros.setId(tvVaros.generateViewId());
+                tvVaros.setTextSize(dpToPx(10));
+
+                TextView tvIdopont = new TextView(mContext);
+                RelativeLayout.LayoutParams paramsIdopont = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                paramsIdopont.addRule(RelativeLayout.BELOW, tvVaros.getId());
+                paramsIdopont.topMargin = dpToPx(15);
+                tvIdopont.setLayoutParams(paramsIdopont);
+                tvIdopont.setText(idopont.substring(0, 16).replace('-', '.'));
+                tvIdopont.setTypeface(getActivity().getResources().getFont(R.font.regular));
+                tvIdopont.setTextColor(getActivity().getResources().getColor(R.color.gray));
+                tvIdopont.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                tvIdopont.setId(tvIdopont.generateViewId());
+                tvIdopont.setTextSize(dpToPx(7));
+
+                TextView tvIdotartam = new TextView(mContext);
+                RelativeLayout.LayoutParams paramsIdotartam = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                paramsIdotartam.addRule(RelativeLayout.BELOW, tvIdopont.getId());
+                paramsIdotartam.topMargin = dpToPx(15);
+                tvIdotartam.setLayoutParams(paramsIdotartam);
+                tvIdotartam.setText(idotartamAtalakitas(idotartam));
+                tvIdotartam.setTypeface(getActivity().getResources().getFont(R.font.regular));
+                tvIdotartam.setTextColor(getActivity().getResources().getColor(R.color.gray));
+                tvIdotartam.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                tvIdotartam.setId(tvIdotartam.generateViewId());
+                tvIdotartam.setTextSize(dpToPx(7));
+
+                TextView tvUles = new TextView(mContext);
+                RelativeLayout.LayoutParams paramsUles = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                paramsUles.addRule(RelativeLayout.BELOW, tvIdotartam.getId());
+                paramsUles.topMargin = dpToPx(20);
+                tvUles.setLayoutParams(paramsUles);
+                tvUles.setText("Ülésszám: " + ules);
+                tvUles.setTypeface(getActivity().getResources().getFont(R.font.regular));
+                tvUles.setTextColor(getActivity().getResources().getColor(R.color.gray));
+                tvUles.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                tvUles.setTextSize(dpToPx(8));
+
+                rlCard.addView(tvVaros);
+                rlCard.addView(tvIdopont);
+                rlCard.addView(tvIdotartam);
+                rlCard.addView(tvUles);
+                card.addView(rlCard);
+                mRelativeLayout.addView(card);
+            }
+        }
+        else {
+            TextView tv = new TextView(mContext);
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+            params.addRule(RelativeLayout.BELOW, R.id.textCim);
+            params.topMargin = dpToPx(200);
+            tv.setLayoutParams(params);
+            tv.setTypeface(getActivity().getResources().getFont(R.font.regular));
+            tv.setTextColor(getActivity().getResources().getColor(R.color.gray));
+            tv.setTextSize(dpToPx(11));
+            tv.setText("Még nincs foglalt járat.");
+            tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            mRelativeLayout.addView(tv);
+        }
+    }
+
+    public int dpToPx(int dp) {
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics()));
+    }
+
+    public String idotartamAtalakitas(String idotartam) {
+        String[] idoresz = idotartam.split(":");
+        if (Integer.parseInt(idoresz[0]) < 10) {
+            return idoresz[0].substring(1, 2) + " óra " + idoresz[1] + " perc";
+        }
+        else {
+            return idoresz[0] + " óra " + idoresz[1] + " perc";
+        }
     }
 }
