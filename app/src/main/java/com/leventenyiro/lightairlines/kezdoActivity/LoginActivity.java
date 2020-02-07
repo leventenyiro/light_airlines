dc0c088472840b1c38020363550fbd926b587fd1
leventenyiro <nyiro.levente@gmail.com>
2020-02-07 16:14:33 +0100

    Login módosítások

--- app/src/main/java/com/leventenyiro/lightairlines/kezdoActivity/LoginActivity.java
+++ app/src/main/java/com/leventenyiro/lightairlines/kezdoActivity/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.leventenyiro.lightairlines.kezdoActivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,6 +8,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -16,6 +18,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.leventenyiro.lightairlines.R;
 import com.leventenyiro.lightairlines.adminActivity.*;
 import com.leventenyiro.lightairlines.segedOsztaly.*;
@@ -24,8 +36,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private AlertDialog alertDialog;
     private AlertDialog.Builder alertDialogBuilder;
     private Button btnLogin;
-    private Database db;
     private EditText inputUsernameEmail, inputPassword;
+    private FirebaseAuth mAuth;
     private int dp15, dp20;
     private Metodus m;
     private TextView btnReg;
@@ -67,10 +79,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         btnLogin = findViewById(R.id.btnLogin);
         inputUsernameEmail = findViewById(R.id.inputUsernameEmail);
         inputPassword = findViewById(R.id.inputPassword);
-        db = new Database(this);
         m = new Metodus(this);
         dp15 = m.dpToPx(15, getResources());
         dp20 = m.dpToPx(20, getResources());
+        mAuth = FirebaseAuth.getInstance();
 
         alertDialogBuilder = new AlertDialog.Builder(LoginActivity.this);
         alertDialogBuilder.setTitle(getString(R.string.exit));
@@ -94,12 +106,19 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         Intent intent;
         switch (v.getId()) {
             case R.id.btnReg:
-                intent = new Intent(LoginActivity.this, Reg1Activity.class);
-                startActivity(intent);
-                finish();
-                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                if (!isNetworkConnected())
+                    Toast.makeText(this, "Nincs internetkapcsolat!", Toast.LENGTH_SHORT).show();
+                else {
+                    intent = new Intent(LoginActivity.this, Reg1Activity.class);
+                    startActivity(intent);
+                    finish();
+                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                }
                 break;
             case R.id.btnLogin:
+                if (!isNetworkConnected())
+                    Toast.makeText(this, "Nincs internetkapcsolat!", Toast.LENGTH_SHORT).show();
+
                 if (inputUsernameEmail.getText().toString().isEmpty()) {
                     Toast.makeText(this, getString(R.string.noUsernameEmail), Toast.LENGTH_LONG).show();
                     inputPassword.setText("");
@@ -109,25 +128,26 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     Toast.makeText(this, getString(R.string.noPassword), Toast.LENGTH_LONG).show();
                     inputSzin("passwordRed");
                 }
-                else if (login() == 0) {
-                    Toast.makeText(LoginActivity.this, getString(R.string.wrongLoginData), Toast.LENGTH_LONG).show();
-                    inputPassword.setText("");
-                    inputSzin("usernameEmailRed");
-                    inputSzin("passwordRed");
-                }
                 else {
-                    if (login() == 1) {
-                        intent = new Intent(LoginActivity.this, WelcomeActivity.class);
-                        startActivity(intent);
-                        finish();
-                        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-                    }
-                    else {
-                        intent = new Intent(LoginActivity.this, AdminActivity.class);
-                        startActivity(intent);
-                        finish();
-                        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-                    }
+                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("user");
+                    ref.orderByChild("username").equalTo(inputUsernameEmail.getText().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                    if (dataSnapshot.getChildrenCount() == 1)
+                                        login(String.valueOf(snapshot.child("email").getValue()));
+                                }
+                            }
+                            else
+                                login(inputUsernameEmail.getText().toString());
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
                 }
                 break;
         }
@@ -154,7 +174,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
-    public int login() {
+    /*public int login() {
         Cursor eredmeny = db.selectLogin(inputUsernameEmail.getText().toString().trim());
         if (eredmeny.getCount() == 1) {
             while (eredmeny.moveToNext()) {
@@ -170,6 +190,72 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
         }
         return 0;
+    }*/
+
+    private void login(String email) {
+        final String finalEmail = email;
+        mAuth.signInWithEmailAndPassword(email, inputPassword.getText().toString())
+            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        if (finalEmail.equals("admin@lightairlines.com")) {
+                            loginIntent(finalEmail);
+                        }
+                        else if (!user.isEmailVerified()) {
+                            user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(LoginActivity.this, "Erősítsd meg az emailed!", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this, "Be vagy jelentkezve!", Toast.LENGTH_SHORT).show();
+                            loginIntent(finalEmail);
+                        }
+                    }
+                    else {
+                        Toast.makeText(LoginActivity.this, getString(R.string.wrongLoginData), Toast.LENGTH_LONG).show();
+                        inputPassword.setText("");
+                        inputSzin("usernameEmailRed");
+                        inputSzin("passwordRed");
+                    }
+                }
+            });
+    }
+
+    private void loginIntent(String email) {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("user");
+        ref.orderByChild("email").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    getSharedPreferences("variables", Context.MODE_PRIVATE).edit()
+                            .putString("userId", snapshot.getKey()).apply();
+                    if (snapshot.getKey().equals("1")) {
+                        Intent intent = new Intent(LoginActivity.this, AdminActivity.class);
+                        startActivity(intent);
+                        finish();
+                        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                    }
+                    else {
+                        Intent intent = new Intent(LoginActivity.this, WelcomeActivity.class);
+                        startActivity(intent);
+                        finish();
+                        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
+        });
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
     }
 
     @Override
